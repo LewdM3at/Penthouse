8b8e15c0fd85a642524d2eecc337c322c45d948b
LewdM3at <angkunster@hotmail.com>
2026-05-01 16:34:10 +0200

    added --random-mac flag to wifite

--- commands/wifi.py
+++ commands/wifi.py
@@ -66,27 +66,27 @@ def _run_interactive(cmd: str):
 
 def auto_audit():
     _clear_screen()
-    _run_interactive(f"sudo wifite")
+    _run_interactive(f"sudo wifite --random-mac")
     return True
 
 def wifite_handshake():
     _clear_screen()
-    _run_interactive(f"sudo wifite --no-pmkid --no-wps --skip-crack")
+    _run_interactive(f"sudo wifite --no-pmkid --no-wps --skip-crack --random-mac")
     return True
 
 def wifite_pmkid():
     _clear_screen()
-    _run_interactive(f"sudo wifite --pmkid --skip-crack")
+    _run_interactive(f"sudo wifite --pmkid --skip-crack --random-mac")
     return True
 
 def wifite_pixiedust():
     _clear_screen()
-    _run_interactive(f"sudo wifite --pixie --no-pmkid --wps-only")
+    _run_interactive(f"sudo wifite --pixie --no-pmkid --wps-only --random-mac")
     return True
 
 def wifite_pin():
     _clear_screen()
-    _run_interactive(f"sudo wifite --no-pixie --no-pmkid --wps-only")
+    _run_interactive(f"sudo wifite --no-pixie --no-pmkid --wps-only --random-mac")
     return True
 
 # ─────────────────────────────────────────── Kismet ───────────────────
